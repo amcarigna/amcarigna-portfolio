7fb07b3b2aef59880f0992de3c1460874cc431bd
amcarigna <amcarigna@gmail.com>
2026-09-18 15:25:18 -0400

    Fix import and app reference in test_app.py

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,10 +1,10 @@
-from ..app import app
+from .. import app
 
 
 def test_home_page():
-    app.config["TESTING"] = True
+    app.app.config["TESTING"] = True
 
-    with app.test_client() as client:
+    with app.app.test_client() as client:
         response = client.get("/")
 
     assert response.status_code == 200
