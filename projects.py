e65f7d82ff44f7930209b46856a0f00d2f65a192
acarigna <amcarigna@gmail.com>
2026-09-21 16:20:18 -0400

    added more projects

--- projects.py
+++ projects.py
@@ -1,5 +1,37 @@
 projects = [
     {
+        "image": "/static/images/book.png",
+        "name": "Book Analysis with Pandas",
+        "techs": [
+            "Python",
+            "Pandas",
+            "Jupyter Notebook"
+        ],
+        "description": """In this project, I got some practice in using pandas to query a .csv database for information
+        about a kaggle dataset from goodreads. I analyzed the database to discover: the most popular book, the relative
+        popularity of long and short books, which reviews can be trusted, what it means for a book to be considered
+        'good,' which authors are the most prolific, and book statistics (length, publisher, published date, etc).""",
+        #"live": "https://python-portfolio-f63j.onrender.com/",
+        "repo": "https://github.com/amcarigna/pandas-book-analysis"
+    },
+    {
+        "image": "/static/images/cleaning.png",
+        "name": "Data Cleaning with Pandas",
+        "techs": [
+            "Python",
+            "Pandas",
+            "Jupyter Notebook"
+        ],
+        "description": """In this project, I cleaned up a small dataset containing a list of pokemon and their
+        attributes. The .csv file had many mistakes and errors to be cleaned, including: bad data types (strings in
+        numeric columns and vice versa), duplicate data, missing data, bad data formatting (e.g., wrong case or bad
+        punctuation), typos, and hanging whitespace. Then I saved the cleaned up dataset to a new .csv file. In order to
+        do this, I also created functions to search the dataframe a little more easily, and a function to move rows
+        around to preserve the order of the pokemon after deletions.""",
+        #"live": "https://python-portfolio-f63j.onrender.com/",
+        "repo": "https://github.com/amcarigna/pandas-data-cleaning"
+    },
+    {
         "image": "/static/images/portfolio.png",
         "name": "Portfolio",
         "techs": [
@@ -15,15 +47,33 @@ projects = [
         "repo": "https://github.com/amcarigna/amcarigna-portfolio"
     },
     {
+        "image": "/static/images/power_rankings.png",
+        "name": "Power-Rankings",
+        "techs": [
+            "Python",
+            "Pandas",
+            "NumPy"
+        ],
+        "description": """A python application for generating power rankings for a sports league. Currently in
+        development. Developed with using the NFL (American Football) in mind. At the moment it functions more like a
+        package due to the fact that it is missing a user interface. I've been using Jupyter Notebook to run functions
+        from it. The necessary functions are all present and work properly. The most important function is:
+        models.build_league(), which will build a computer model of a league for the first time, or load previous league
+        data.""",
+        #"live": "https://python-portfolio-f63j.onrender.com/",
+        "repo": "https://github.com/amcarigna/power-rankings"
+    },
+    {
         "image": "/static/images/mtg.png",
         "name": "MTG-Inventory",
         "techs": [
             "Python"
         ],
         "description": """This project is in early development. The goal of the project is to create an application to
-        add, search, update, and delete trading cards from one's inventory. The purpose of this project is two-fold:
-        One, to be able to store inventory digitally and locally without needing to subscribe to some online service.
-        Two, to see if the features provided by those online services can improved upon.""",
+        add, search, update, and delete trading cards from one's inventory. It uses Scryfall API to get trading card
+        information.The purpose of this project is two-fold: One, to be able to store inventory digitally and locally
+        without needing to subscribe to some online service. Two, to see if the features provided by those online
+        services can improved upon.""",
         #"live": "https://python-portfolio-f63j.onrender.com/",
         "repo": "https://github.com/amcarigna/mtg-inventory"
     },
