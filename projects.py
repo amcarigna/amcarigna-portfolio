051ca21c4c88ddca31f1b3992cac703434684685
acarigna <amcarigna@gmail.com>
2026-09-21 12:49:51 -0400

    added my photo and two projects

--- projects.py
+++ projects.py
@@ -1,5 +1,33 @@
 projects = [
     {
+        "image": "/static/images/portfolio.png",
+        "name": "Portfolio",
+        "techs": [
+            "Python",
+            "HTML",
+            "CSS",
+            "JavaScript"
+        ],
+        "description": """This is the project that created the webpage you are on right now! It uses Flask to create a
+        simple webpage for exhibiting current and prior projects. Most of the work in creating the webpage was done
+        using HTML, but some Python was used, including for the storage of this paragraph.""",
+        "live": "https://python-portfolio-f63j.onrender.com/",
+        "repo": "https://github.com/amcarigna/amcarigna-portfolio"
+    },
+    {
+        "image": "/static/images/mtg.png",
+        "name": "MTG-Inventory",
+        "techs": [
+            "Python"
+        ],
+        "description": """This project is in early development. The goal of the project is to create an application to
+        add, search, update, and delete trading cards from one's inventory. The purpose of this project is two-fold:
+        One, to be able to store inventory digitally and locally without needing to subscribe to some online service.
+        Two, to see if the features provided by those online services can improved upon.""",
+        #"live": "https://python-portfolio-f63j.onrender.com/",
+        "repo": "https://github.com/amcarigna/mtg-inventory"
+    },
+    {
         "image": "/static/images/stargazer-screenshot.png",
         "name": "Stargazer Log",
         "techs": [
@@ -7,7 +35,7 @@ projects = [
             "HTML",
             "CSS"
         ],
-        "description": """This project served as a introduction to using Git and Github. Before this, I was aware
+        "description": """This project served as an introduction to using Git and Github. Before this, I was aware
         generally of version control, why it is, and rough logic behind how it works. But using Git and Github always
         intimidated me, and I avoided it. Now I am comfortable using its basic features and ready to learn other
         ones.""",
